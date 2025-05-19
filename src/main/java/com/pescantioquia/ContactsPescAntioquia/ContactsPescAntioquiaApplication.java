15ea5a1d9f8360a6184cad1a55aadcc41bd8808a
Josimar Sanchez <josanchezag@gmail.com>
2025-05-19 15:51:25 -0500

    Initial project structure

---
+++ src/main/java/com/pescantioquia/ContactsPescAntioquia/ContactsPescAntioquiaApplication.java
@@ -0,0 +1,13 @@
+package com.pescantioquia.ContactsPescAntioquia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsPescAntioquiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsPescAntioquiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pescantioquia/ContactsPescAntioquia/ContactsPescAntioquiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pescantioquia.ContactsPescAntioquia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsPescAntioquiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
